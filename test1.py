cb9f95520d2e90259f39462a4f422a337b9b18a9
anandshekhr <66034323+anandshekhr@users.noreply.github.com>
2022-08-30 22:21:51 +0530

    Updates
    Add thumbnail column in html
    add Thumbnail in dictionary
    add upto 50 video check

--- test1.py
+++ test1.py
@@ -5,8 +5,7 @@ from flask_cors import CORS, cross_origin
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as urReq
 from selenium import webdriver
-import requests
-import mysql, pymongo
+import requests, mysql, pymongo, time
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -38,6 +37,8 @@ def searchKeyString():
             searchText = request.form.get('searchtext').replace(" ","+")
             url = ("https://www.youtube.com/results?search_query={}".format(searchText))
             driver.get(url)
+            driver.execute_script("window.scrollTo(0,2000);")
+            time.sleep(1)
             content = driver.page_source.encode('utf-8').strip()
             soup = bs(content,'html.parser')
             findChannelSection = soup.find_all('div',id="content-section")
@@ -45,15 +46,20 @@ def searchKeyString():
                 channelLink = tag.div.a['href']
             channelEmbeddedURL = "https://www.youtube.com{}/videos?view=0&sort=dd&flow=grid".format(channelLink)
             driver.get(channelEmbeddedURL)
+            driver.execute_script("window.scrollTo(0,2000);")
+            time.sleep(1)
             findChannelPage = driver.page_source.encode('utf-8').strip()
             pageSoup = bs(findChannelPage,'lxml')
             titles = pageSoup.findAll('a',id="video-title")
             views = pageSoup.findAll('span',class_="style-scope ytd-grid-video-renderer")
+            thumbnail = pageSoup.findAll('img',class_="style-scope yt-img-shadow")
+            thumb_list = []
+            for thumb in thumbnail[6:]:
+                thumb_list.append(thumb.get('src'))
             channel_details = []
             i, j = 0, 0
-            for title in titles:
-                d = {"videoTitle":title.text,"totalViews":views[i].text,"videoURL":"https://www.youtube.com"+title.get('href')}
-                # print(d)
+            for title in titles[:50]:
+                d = {"videoTitle":title.text,"totalViews":views[i].text,"videoURL":"https://www.youtube.com"+title.get('href'),"thumbnail":thumb_list[j]}
                 channel_details.append(d)
                 i +=2
                 j +=1
@@ -64,6 +70,11 @@ def searchKeyString():
             return "something went wrong"
     else:
         return render_template("home.html")
+
+
+
+
+
 
 
 if __name__ == "__main__":
